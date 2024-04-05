532dace30633637a65e7acb39eae16be4b422be9
Ades-B <adrian_banico@dlsu.edu.ph>
2024-04-05 16:07:56 +0800

    Added descriptions to visualizatons
    Added descriptions before the Map and Line visualizations, and edited some some stuff to generally improve readability

--- app.py
+++ app.py
@@ -96,7 +96,8 @@ app.layout = dbc.Container([
         # Landing Page Text and Images
         dbc.Row([
                 dbc.Col([
-                    html.H5("Dengue is not Uncommon in the Philippines"),
+                    html.H5("Dengue is not Uncommon in the Philippines",
+                            style={"color": "black"}),
                     html.P("Every year, tens and even hundreds of thousands of Filipinos contract this illness, and many succumb to it. The 2019 dengue outbreak was one of the worst years for the country, wherein it has reported the highest number of cases and deaths across Southeast Asia (Santos, 2019). The impact was severe, with over 370,000 cases, and 1400 casualties recorded in a single year (World Health Organization, 2019).", style={
                         'text-align': 'justify'})
                 ], width=8),
@@ -115,7 +116,8 @@ app.layout = dbc.Container([
                              style={'width': '100%'}),
                 ], width=4),
                 dbc.Col([
-                    html.H5("The Vital Role of Accessible Healthcare"),
+                    html.H5("The Vital Role of Accessible Healthcare",
+                            style={"color": "black"}),
                     html.P("The World Health Organization (2022) emphasizes that timely medical care and early detection can reduce dengue fatality rates to less than 1%. Research by Lum et al. (2014) underscores the significance of basic primary care access in mitigating dengue-related morbidity and mortality. Freitas et al. (2019) further support this, revealing that inadequate healthcare access leads to delayed treatment, with 90% of dengue fatalities occurring due to late admission to medical facilities in areas with limited healthcare infrastructure.", style={
                         'text-align': 'justify'}),
                 ], width=8),
@@ -125,7 +127,8 @@ app.layout = dbc.Container([
 
         dbc.Row([
                 dbc.Col([
-                    html.H5("Our goal for this Project:"),
+                    html.H5("Our goal for this Project:",
+                            style={"color": "black"}),
                     html.P("Our project aims to shed light on the severity of Dengue in the regions and cities of the Philippines. Through visualizations - the use of graphs, charts, and maps, we will illustrate the incidence of dengue cases, deaths, and fatality rates across different cities, allowing for comparisons to identify the most and least affected areas. Additionally, we intend to identify notable regions with high and low dengue fatality rates to establish focal points for further investigation and intervention efforts.  By utilizing data visualization techniques, we aim to not only raise awareness about the pressing healthcare challenges faced by certain communities but also to inform targeted interventions aimed at mitigating the impact of dengue outbreaks in the Philippines.", style={
                         'text-align': 'justify'})
                 ], width=8),
@@ -135,7 +138,26 @@ app.layout = dbc.Container([
                 ], width=4),
                 ]),
 
+        html.Br(),
+
         html.Hr(),
+
+        dbc.Row([
+            html.Div([
+                html.H4("Severity of Dengue Across the Philippines", style={
+                    'text-align': 'left',
+                    "padding-top": '60px',
+                    "padding-bottom": '10px',
+                    "color": "black"
+
+                }),
+                html.P("The interactive map on the bottom left presents the severity of dengue in the Philippines. Select the depth of the data whether regional or provincial on the radio buttons. There is also an option on selecting which variable you wish to access. There are options of obtaining dengue cases, deaths, and fatality rates. The bar graph on the bottom right depicts the same data but provides a point of comparison between the areas in the country", style={
+                    'text-align': 'justify',
+                    "padding-bottom": '15px'
+                })
+            ])
+        ]),
+
 
         # Visualizations
         dbc.Row([
@@ -191,6 +213,21 @@ app.layout = dbc.Container([
 
         html.Br(),
         html.Hr(),
+        dbc.Row([
+            html.Div([
+                html.H4("Dengue Severity Across Time", style={
+                    'text-align': 'left',
+                    "padding-top": '60px',
+                    "padding-bottom": '10px',
+                    "color": "black"
+
+                }),
+                html.P("The interactive line graph below presents the reported severity of Dengue in the Philippines across the months in 2020. Select the depth of the data whether national or regional on the radio buttons. There is also an option on selecting which variables and regions you wish to access. There are options of obtaining dengue cases and deaths by selecting the radio buttons, and the dropdown provides a method of isolating a specific region that you wish to highlight. Additionally, the legend of on the right is interactive. Clicking on a region allows the attributed line to disappear or reappear. Selecting National as the depth aggregates the reported severity across all regions in the country throughout the year.", style={
+                    'text-align': 'justify',
+                    "padding-bottom": '15px'
+                })
+            ])
+        ]),
         # Line Chart - Insert words here
         html.Div([
             dcc.Graph(
